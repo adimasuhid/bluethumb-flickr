1c56f88b2c135a0080c72cc66e7c350999ec32c7
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-04-19 12:54:10 +0800

    Add base search ui implementation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,16 @@
 class SearchController < ApplicationController
   def index
+    if search_params[:q].present?
+      client = PhotoClient.new
+      @results = client.search(search_params[:q])
+    else
+      @results = []
+    end
+  end
 
+  private
+
+  def search_params
+    params.permit(:q)
   end
 end
