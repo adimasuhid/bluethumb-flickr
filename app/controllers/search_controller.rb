1940f011450526a820541ad9418b4bf828c8aa10
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-04-19 14:58:11 +0800

    Update search integration spec to include failed connection message

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,8 @@ class SearchController < ApplicationController
     if search_params[:q].present?
       client = PhotoClient.new
       @results = client.search(search_params[:q])
+
+      flash[:error]= "Search failed to connect" if !client.last_request_success?
     else
       @results = []
     end
--- spec/integrations/search_spec.rb
+++ spec/integrations/search_spec.rb
@@ -12,8 +12,7 @@ feature 'User searches photos' do
 
   context 'with non-empty string' do
     scenario 'with results' do
-      dummy_client = double(search: [double(source: 'http://test_url.jpg')])
-      allow(PhotoClient).to receive(:new).and_return(dummy_client)
+      use_dummy_client(results: [double(source: 'http://test_url.jpg')])
 
       visit root_path
       fill_in 'q', with: 'dog'
@@ -25,9 +24,7 @@ feature 'User searches photos' do
     end
 
     scenario 'with no results' do
-      dummy_client = double(search: [])
-      allow(PhotoClient).to receive(:new).and_return(dummy_client)
-
+      use_dummy_client
       visit root_path
       fill_in 'q', with: '23904850498539840932340'
       click_button 'Search'
@@ -36,5 +33,21 @@ feature 'User searches photos' do
       expect(page).to_not have_text('Start searching')
       expect(page).to have_text('No results found')
     end
+
+    scenario 'with failed connection to client API' do
+      use_dummy_client(success: false)
+      visit root_path
+      fill_in 'q', with: '23904850498539840932340'
+      click_button 'Search'
+
+      expect(page).to_not have_css('img')
+      expect(page).to have_text('No results found')
+      expect(page).to have_text('Search failed to connect')
+    end
+  end
+
+  def use_dummy_client(results: [], success: true)
+    dummy_client = double(search: results, last_request_success?: success)
+    allow(PhotoClient).to receive(:new).and_return(dummy_client)
   end
 end
